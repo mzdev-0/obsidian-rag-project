7f2930a7c2eb5e03cd18e80a8af410905b4c2cba
mzdev-0 <github@ghostfrnd.co>
2025-07-11 17:59:54 -0500

    update gitignore

--- embed.py
+++ embed.py
@@ -1,18 +1,41 @@
 # Requires transformers>=4.51.0
 # Requires sentence-transformers>=2.7.0
-
+import torch
 from sentence_transformers import SentenceTransformer
+from transformers.utils.quantization_config import BitsAndBytesConfig
+
+# empty the cache
+torch.cuda.empty_cache()
+
+# Set the memory fraction to 40% of the total GPU memory.
+# Adjust the 0.4 value as needed for your model and hardware.
+# torch.cuda.set_per_process_memory_fraction(0.7, 0)
 
 # Load the model
-model = SentenceTransformer("Qwen/Qwen3-Embedding-4B")
+#
+# model = SentenceTransformer("Qwen/Qwen3-Embedding-4B")
 
+# quantization_config = BitsAndBytesConfig(
+#    load_in_4bit=True,
+#    bnb_4bit_quant_type="nf4",  # Normal Float 4-bit
+#    bnb_4bit_compute_dtype=torch.float16,  # Compute in fp16
+#    bnb_4bit_use_double_quant=True,  # Double quantization for extra savings
+# )
 # We recommend enabling flash_attention_2 for better acceleration and memory saving,
 # together with setting `padding_side` to "left":
-# model = SentenceTransformer(
-#     "Qwen/Qwen3-Embedding-8B",
-#     model_kwargs={"attn_implementation": "flash_attention_2", "device_map": "auto"},
-#     tokenizer_kwargs={"padding_side": "left"},
-# )
+model = SentenceTransformer(
+    "Qwen/Qwen3-Embedding-8B-GGUF",
+    model_kwargs={
+        "file_name": "qwen3-embedding-8b-Q4_K_M",
+        #        "quantization_config": quantization_config,
+        "attn_implementation": "flash_attention_2",
+        "device_map": "auto",
+        "torch_dtype": torch.float16,
+        "low_cpu_mem_usage": True,
+    },
+    tokenizer_kwargs={"padding_side": "left"},
+    device=None,
+)
 
 # The queries and documents to embed
 queries = [
