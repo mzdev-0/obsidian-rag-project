913b07c1c9f795213388a298fd26874fb608b89c
mzdev-0 <github@ghostfrnd.co>
2025-07-11 12:39:14 -0500

    swap to sentence-transformers for initial testing

--- embed.py
+++ embed.py
@@ -1,28 +1,37 @@
-# Requires vllm>=0.8.5
-import torch
-import vllm
-from vllm import LLM
+# Requires transformers>=4.51.0
+# Requires sentence-transformers>=2.7.0
 
+from sentence_transformers import SentenceTransformer
 
-def get_detailed_instruct(task_description: str, query: str) -> str:
-    return f"Instruct: {task_description}\nQuery:{query}"
+# Load the model
+model = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")
 
+# We recommend enabling flash_attention_2 for better acceleration and memory saving,
+# together with setting `padding_side` to "left":
+# model = SentenceTransformer(
+#     "Qwen/Qwen3-Embedding-0.6B",
+#     model_kwargs={"attn_implementation": "flash_attention_2", "device_map": "auto"},
+#     tokenizer_kwargs={"padding_side": "left"},
+# )
 
-# Each query must come with a one-sentence instruction that describes the task
-task = "Given a web search query, retrieve relevant passages that answer the query"
+# The queries and documents to embed
 queries = [
-    get_detailed_instruct(task, "What is the capital of China?"),
-    get_detailed_instruct(task, "Explain gravity"),
+    "What is the capital of China?",
+    "Explain gravity",
 ]
-# No need to add instruction for retrieval documents
 documents = [
     "The capital of China is Beijing.",
     "Gravity is a force that attracts two bodies towards each other. It gives weight to physical objects and is responsible for the movement of planets around the sun.",
 ]
-input_texts = queries + documents
-model = LLM(model="Qwen/Qwen3-Embedding-8B", task="embed")
-outputs = model.embed(input_texts)
-embeddings = torch.tensor([o.outputs.embedding for o in outputs])
-scores = embeddings[:2] @ embeddings[2:].T
-print(scores.tolist())
-# [[0.7482624650001526, 0.07556197047233582], [0.08875375241041183, 0.6300010681152344]]
+
+# Encode the queries and documents. Note that queries benefit from using a prompt
+# Here we use the prompt called "query" stored under `model.prompts`, but you can
+# also pass your own prompt via the `prompt` argument
+query_embeddings = model.encode(queries, prompt_name="query")
+document_embeddings = model.encode(documents)
+
+# Compute the (cosine) similarity between the query and document embeddings
+similarity = model.similarity(query_embeddings, document_embeddings)
+print(similarity)
+# tensor([[0.7646, 0.1414],
+#         [0.1355, 0.6000]])
